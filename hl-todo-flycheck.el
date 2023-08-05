8b3d547f8fd022a496ff638c5cf7e6a3d5b63d7a
alvaro@thinkpad <alvaro@thinkpad>
2023-08-05 18:10:00 +0200

    better packaging

--- hl-todo-flycheck.el
+++ hl-todo-flycheck.el
@@ -1,7 +1,7 @@
-;;; hl-todo-flycheck.el --- Display hl-todo keywords in flycheck.  -*- lexical-binding: t; -*-
+;;; hl-todo-flycheck.el --- Display hl-todo keywords in flycheck  -*- lexical-binding: t; -*-
 
 ;; Author: Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
-;; URL: https://github.com/alvarogonzalezsotillo/hl-todo-flycheck
+;; Homepage: https://github.com/alvarogonzalezsotillo/hl-todo-flycheck
 ;; Package-Requires: ((emacs "25.1") (hl-todo) (flycheck))
 ;; Version: 1.0
 ;; Keywords: convenience
@@ -20,8 +20,6 @@
 ;;
 ;; Configure `hl-todo' and `flycheck'.
 ;; Invoke `hl-todo-flycheck-enable' and open flyckeck error list.
-;; Based on https://emacs.stackexchange.com/questions/29496/automatically-run-org-lint-through-flycheck
-;; TODO: test with flycheck-projectile-list-errors
 
 (require 'flycheck)
 (require 'hl-todo)
@@ -42,8 +40,6 @@ Return a list of (position text id)."
           (goto-char (point-min))
           (let ((case-fold-search nil)) ; Only exact case in search
             (while (re-search-forward regex nil t)
-              ;;(message "buscando en:%s" (point))word
-
               (let* ((pos (point))
                      (id (thing-at-point 'symbol))
                      (bol (line-beginning-position))
@@ -56,14 +52,12 @@ Return a list of (position text id)."
 (defun hl-todo-flycheck--start (checker callback)
   "Start function of hl-todo checker.
 CHECKER and CALLBACK are documented in `flycheck-define-generic-checker'."
-  ;;(message "hl-todo-flycheck--start")
   (funcall
    callback 'finished
    (mapcar (lambda (pos-msg-id)
              (let ((pos (nth 0 pos-msg-id))
                    (msg (nth 1 pos-msg-id))
                    (id  (nth 2 pos-msg-id)))
-               ;;(message "nuevo error:%s %s" pos msg)
                (flycheck-error-new-at-pos pos 'info msg :id id :checker checker)))
            (hl-todo-flycheck--occur-to-error))))
 
@@ -89,21 +83,25 @@ CHECKER and CALLBACK are documented in `flycheck-define-generic-checker'."
   "List of checkers to not be augmented with hl-todo."
   :type 'list)
 
+;; Create hl-todo checker
+(flycheck-define-generic-checker 'hl-todo
+  "Syntax checker for hl-todo."
+  :start '#hl-todo-flycheck--start
+  :modes '(text-mode))
+
+
 ;;;###autoload
 (defun hl-todo-flycheck-enable ()
   "Install and enable hl-todo-flycheck."
   (interactive)
 
-  ;; Create hl-todo checker
-  (flycheck-define-generic-checker 'hl-todo
-    "Syntax checker for hl-todo."
-    :start 'hl-todo-flycheck--start
-    :modes (hl-todo-flycheck--get-all-modes))
-
   ;; Register hl-todo checker, and enable it
   (add-to-list 'flycheck-checkers 'hl-todo)
-  ;;(flycheck-disable-checker 'hl-todo t)
   (setq flycheck-disabled-checkers (delete 'hl-todo flycheck-disabled-checkers))
+
+  ;; Add all modes to hl-todo checker
+  (dolist (checker (hl-todo-flycheck--get-all-modes))
+    (flycheck-add-mode 'hl-todo checker))
   
   ;; Chain hl-todo checker to all existing checkers, except disabled modes, and self
   (dolist (checker flycheck-checkers)
@@ -119,8 +117,9 @@ CHECKER and CALLBACK are documented in `flycheck-define-generic-checker'."
 (defun hl-todo-flycheck-disable ()
   "Disable hl-todo-flycheck."
   (interactive)
-  ;;(flycheck-disable-checker 'hl-todo nil)
   (add-to-list 'flycheck-disabled-checkers 'hl-todo)
+
+  ;; Force flycheck update
   (flycheck-buffer))
 
 (provide 'hl-todo-flycheck)
